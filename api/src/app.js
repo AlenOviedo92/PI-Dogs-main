6a1a5d86637e3eeec0ba413cc915c6359b870d2c
Alen Oviedo <afoviedola@gmail.com>
2024-02-20 15:32:52 -0500

    add cors

--- api/src/app.js
+++ api/src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors');
 const routes = require('./routes/index.js');
 require('dotenv').config();
 const { FRONTEND_URL } = process.env;
@@ -16,7 +17,8 @@ server.name = 'API';
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));               //el .use() le indica a la request entrar o pasar por lo que está entre ()
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
-server.use(morgan('dev'));                                                          
+server.use(morgan('dev'));
+server.use(cors());                                                          
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', `${FRONTEND_URL}`);               //update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
